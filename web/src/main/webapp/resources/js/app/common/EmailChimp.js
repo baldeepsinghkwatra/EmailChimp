d0b097cf21c61d7250981e3ecb89b2e353bd2f18
Anshul Gupta <anshulgupta231193@gmail.com>
2016-10-11 13:19:08 +0530

    Login Signup Dynamic Js Loading.

--- web/src/main/webapp/resources/js/app/common/EmailChimp.js
+++ web/src/main/webapp/resources/js/app/common/EmailChimp.js
@@ -34,13 +34,36 @@ var EmailChimp = {
                 {type: 'left', size: 200, resizable: false, style: StyleConstant.layout},
                 {type: 'main', resizable: true, style: StyleConstant.layout, content: 'main', title: 'Outbox'},
                 {type: 'bottom', size: 30, resizable: false, style: StyleConstant.layout},
-            ]
+            ],
+            onShow: function(event) {
+                if(!EmailChimp.conf.headerPanel){
+                    w2ui['layout'].content('title', 'hi');
+                    w2ui['layout'].hide('top',true);
+                }
+                if(!EmailChimp.conf.LeftPanel){
+                    w2ui['layout'].hide('left',true);
+                }
+                if(!EmailChimp.conf.toolbar){
+                    w2ui['layout'].hide('bottom',true);
+                }
+            }
+        });
+        w2ui.layout.on('show', function(event) {
+            console.log('object '+ event.panel + ' is shown');
         });
     },
     initializePanel: function () {
-        Object.create(HeaderPanel).init();
-        Object.create(LeftPanel).init();
-        Object.create(FooterPanel).init();
+        if(!EmailChimp.conf.headerPanel){
+            w2ui['layout'].get('main').title = '';
+            w2ui['layout'].hide('top',true);
+        }
+        if(!EmailChimp.conf.LeftPanel){
+            w2ui['layout'].hide('left',true);
+        }
+        if(!EmailChimp.conf.toolbar){
+            w2ui['layout'].hide('bottom',true);
+        }
+
     },
     openPopUp: function (conf, callback) {
         $().w2popup('open', this.getPopup(conf, callback));
@@ -81,7 +104,6 @@ var EmailChimp = {
         };
     },
     loadComponent: function (package,component) {
-
         if(window[component] == undefined){
             $.getScript('resources/js/app/'+package+'/'+component+'.js', function () {
                     window[component].init();
