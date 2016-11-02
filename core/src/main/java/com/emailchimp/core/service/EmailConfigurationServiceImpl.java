8ef1e374056256665dd66bdda7b690e4778a0e46
mfsi-baldeep <baldeepk@mindfiresolutions.com>
2016-11-02 13:00:04 +0530

    Logout

---
+++ core/src/main/java/com/emailchimp/core/service/EmailConfigurationServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * ****************************************************************************
+ *
+ * Copyright (c) 2016, Mindfire Solutions and/or its affiliates. All rights
+ * reserved.
+ * ___________________________________________________________________________________
+ *
+ *
+ * NOTICE: All information contained herein is, and remains the property of
+ * Mindfire and its suppliers,if any. The intellectual and technical concepts
+ * contained herein are proprietary to Mindfire Solutions. and its suppliers and
+ * may be covered by us and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Mindfire Solutions
+ */
+package com.emailchimp.core.service;
+
+import com.emailchimp.core.dao.EmailConfigurationDAO;
+import com.emailchimp.core.model.EmailConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author baldeep
+ */
+@Service
+@Transactional
+public class EmailConfigurationServiceImpl extends CommonServiceImpl<EmailConfiguration> implements EmailConfigurationService {
+
+    EmailConfigurationDAO emailConfigurationDAO;
+
+    @Autowired
+    public EmailConfigurationServiceImpl(EmailConfigurationDAO emailConfigurationDAO) {
+        super(emailConfigurationDAO);
+        this.emailConfigurationDAO = emailConfigurationDAO;
+    }
+}
